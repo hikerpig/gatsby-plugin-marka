024b9a8f1f4f68bb4b2b835aff1b4b6a3d3c4fa5
hikerpig <hikerpigwinnie@gmail.com>
2020-10-26 18:16:45 +0800

    feat: first version done

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,42 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-// You can delete this file if you're not using it
+// ---------------- Marka ----------------------
+function markaInit() {
+  if (!window.marka) return
+  marka.init({
+    container: '.post__content',
+  })
+}
+
+let options = {
+  container: '.js-marka-container',
+}
+
+export const onInitialClientRender = function (_o, pluginOptions) {
+  options = {
+    ...options,
+    ...pluginOptions,
+  }
+  const styleLink = document.createElement('link')
+  styleLink.rel = 'stylesheet'
+  styleLink.href = 'https://unpkg.com/marka-js@latest/dist/marka.css'
+  document.head.appendChild(styleLink)
+
+  const script = document.createElement('script')
+  script.src = 'https://unpkg.com/marka-js@latest/dist/marka-js.umd.js'
+  document.body.appendChild(script)
+
+  script.addEventListener('load', function () {
+    marka.onScriptLoaded(this)
+    markaInit(options)
+  })
+}
+
+export const onRouteUpdate = ({ location, prevLocation }) => {
+  if (window.marka) {
+    markaInit(options)
+  } else {
+    window.addEventListener('DOMContentLoaded', function () {
+      markaInit(options)
+    })
+  }
+}
+// ----------------end Marka -------------------
