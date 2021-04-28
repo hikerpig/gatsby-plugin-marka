4c98ca16e159e230461ed00bca7840d61b0bacc9
hikerpig <hikerpigwinnie@gmail.com>
2021-04-28 18:46:06 +0800

    feat: change cdn to jsdelivr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -20,11 +20,11 @@ export const onInitialClientRender = function (_o, pluginOptions) {
   }
   const styleLink = document.createElement('link')
   styleLink.rel = 'stylesheet'
-  styleLink.href = 'https://unpkg.com/marka-js@latest/dist/marka.css'
+  styleLink.href = 'https://cdn.jsdelivr.net/npm/marka-js@latest/dist/marka.css'
   document.head.appendChild(styleLink)
 
   const script = document.createElement('script')
-  script.src = 'https://unpkg.com/marka-js@latest/dist/marka-js.umd.js'
+  script.src = 'https://cdn.jsdelivr.net/npm/marka-js@latest/dist/marka-js.umd.js'
   document.body.appendChild(script)
 
   script.addEventListener('load', function () {
